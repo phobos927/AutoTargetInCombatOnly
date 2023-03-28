1a7440c48ef5dee1c66ee6aa38a20ec6aaf5d5f5
Евгений <30345788+phobos927@users.noreply.github.com>
2023-03-28 23:41:20 +0700

    add settings for hide notification, wow 10.0.7

--- core.lua
+++ core.lua
@@ -1,16 +1,49 @@
 local incombat = UnitAffectingCombat("player")
 local holder = CreateFrame("Frame")
+local loaded = false
+holder:RegisterEvent("ADDON_LOADED");
 holder:RegisterEvent("PLAYER_REGEN_ENABLED")
 holder:RegisterEvent("PLAYER_REGEN_DISABLED")
-holder:SetScript("OnEvent", function(self, event)
-      incombat = (event=="PLAYER_REGEN_ENABLED")
-      local realInCombat = (incombat==false)           
-      if (realInCombat==true)
-      then
-         print('Auto-target ON');
-         ConsoleExec( "SoftTargetEnemy 3")
-      else
-         print('Auto-target OFF');
-         ConsoleExec( "SoftTargetEnemy 0")
-      end
+holder:SetScript("OnEvent", function(self, event, arg1)
+    incombat = (event == "PLAYER_REGEN_ENABLED")
+    if event == "PLAYER_REGEN_ENABLED" or event == "PLAYER_REGEN_DISABLED" then
+        local realInCombat = (incombat == false)
+        if (realInCombat == true) then
+            if (hiddenTextDB == true) then
+                --
+            else
+                print('AutoTarget ON')
+            end
+            ConsoleExec("SoftTargetEnemy 3")
+        else
+            if (hiddenTextDB == true) then
+               --
+            else
+                print('AutoTarget OFF')
+            end
+            ConsoleExec("SoftTargetEnemy 0")
+        end
+    end
+    if event == "ADDON_LOADED" and arg1 == "AutoTargetInCombatOnly" then
+        local myCheckbox = CreateFrame("CheckButton", "AutoTargetInCombatOnly_MyCheckbox", self,
+            "InterfaceOptionsCheckButtonTemplate")
+        myCheckbox:SetPoint("TOPLEFT", 16, -16)
+        myCheckbox.Text:SetText("Hide notification in chat")
+        myCheckbox:SetChecked(hiddenTextDB)
+        -- myCheckbox.tooltipText = "description"
+        myCheckbox:SetScript("OnClick", function(self)
+            local isChecked = self:GetChecked()
+            hiddenTextDB = isChecked
+        end)
+
+        self.name = "AutoTargetInCombatOnly" -- see panel fields
+        InterfaceOptions_AddCategory(self) -- see InterfaceOptions API
+
+        -- add widgets to the panel as desired
+        local title = self:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
+        title:SetPoint("TOP")
+        title:SetText("AutoTargetInCombatOnly")
+    end
 end)
+
+
